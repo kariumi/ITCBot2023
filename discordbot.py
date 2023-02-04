611477cbd40a0d15b9fa460e0b1783be8315b629
kariumi <kariumi1028a@gmail.com>
2023-02-05 00:45:17 +0900

    voteコマンド作成開始

--- discordbot.py
+++ discordbot.py
@@ -27,6 +27,21 @@ async def on_command_error(ctx, error):
         return
     raise error
 
+"""
+!shuffle
+自分が入っているボイスチャンネルの人を指定したボイスチャンネルにランダムに振り分け、自動的に移動させるコマンドです。
+
+!shuffle [ボイスチャンネルID 1] [ボイスチャンネルID 2] ...
+例：!shuffle 123456789012345678 123456789012345679
+上記のように指定すると、指定したボイスチャンネルにランダムに振り分けることができます。
+
+!shuffle [(任意)ロール 1] [(任意)ロール 2] [(任意)ロール 3] [ボイスチャンネルID 1] [ボイスチャンネルID 2] ...
+例：!shuffle @DTM部 @CG部 123456789012345678 123456789012345679
+上記のようにロールを指定すると、指定したロールのメンバーは均等に振り分けられます。
+・ロールは0~3個の間で指定することができます。
+(created by Chino)
+"""
+
 
 @client.command()
 async def shuffle(ctx, host1: typing.Optional[Role] = None, host2: typing.Optional[Role] = None, host3: typing.Optional[Role] = None, *channels: VoiceChannel):
@@ -75,6 +90,48 @@ async def shuffle(ctx, host1: typing.Optional[Role] = None, host2: typing.Option
         await hosts3[i].move_to(channels[i % len(channels)])
 
     await ctx.send(f"{channel.mention}に接続している人を移動させました")
+
+"""
+!vote
+投票を作成するコマンド
+
+!vote create [投票タイトル] [投票先1] [投票先2] [投票先3] ...
+投票を作成してくれます。
+
+!vote list_up [メッセージID]
+投票したメンバーを表示します。
+
+"""
+
+
+@client.command()
+async def vote(ctx, *args):
+    if not ctx.guild.get_role(968160313797136414) in ctx.author.roles:
+        await ctx.send("実行権限がありません")
+        return
+
+    if args[0] == "create":
+        vote_title = args[1]
+        vote_list = []
+        vote_icon = [":one:", ":two:", ":three:", ":four:",
+                     ":five:", ":six:", ":seven:", ":eight:", ":nine:"]
+        for i in range(len(args[1:])):
+            mes += f"{vote_icon[i]} {args[i+1]}\n"
+        message = await ctx.send(f"【投票】"
+                                 f"{vote_title}\n"
+                                 f"\n"
+                                 f"{mes}")
+        for i in range(len(args[1:])):
+            await message.add_reaction(vote_icon[i])
+
+    elif args[0] == "list_up":
+        pass
+    else:
+        await ctx.send("Error:引数が認識されませんでした。\n"
+                       "```\n"
+                       "!vote create [投票タイトル] [投票先1] ...\t投票を作成します。\n"
+                       "!vote list_up [メッセージID]\t投票結果を表示します。"
+                       "```")
 
 
 token = getenv('DISCORD_BOT_TOKEN')
