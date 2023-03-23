152c0b581f244da84c6bb41a5f8faeb921c99b47
kariumi <kariumi1028a@gmail.com>
2023-03-24 04:45:01 +0900

    ds

--- discordbot.py
+++ discordbot.py
@@ -774,13 +774,13 @@ remove - 文字列が一致する行を削除
 
 
 @client.command()
-async def ModifyDatabase(ctx, *arg):
+async def ModifyDatabase(ctx, arg, channel: typing.Optional[TextChannel],  mes, str):
     guild = client.get_guild(1075592226534600755)
-    channel = guild.get_channel(arg[1])
-    message = channel.fetch_message(arg[2])
+    # channel = guild.get_channel(arg[1])
+    message = channel.fetch_message(mes)
     content = message.content
-    if arg[0] == "add":
-        content += f"\n{arg[3]}"
+    if arg == "add":
+        content += f"\n{str}"
         await message.edit(content=content)
 
 
@@ -815,7 +815,7 @@ async def Trial_entry_explulsion():
 
     message += f"**BOTの最新データ** \n"
 
-    message += f"最終更新日時：2023/3/24 2:51\n"
+    message += f"最終更新日時：2023/3/24 4:44\n"
 
     message += f"UTC時間：{now_time.year}/{now_time.month}/{now_time.day} {now_time.hour}:{now_time.minute}:{now_time.second}\n"
 
