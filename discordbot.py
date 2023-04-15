c6cd528a888cbdf6b3e8751bbf2363eea5b8eaa3
kariumi <kariumi1028a@gmail.com>
2023-04-15 17:11:26 +0900

    a

--- discordbot.py
+++ discordbot.py
@@ -850,7 +850,8 @@ async def Trial_entry_explulsion():
 
         await DBmessage.edit(content=message)  # ログ
     except Exception as e:
-        await printLog(client, failure(e))
+        pass
+        # await printLog(client, failure(e))
 
 
 """
