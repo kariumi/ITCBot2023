44d35648fd6bcddae8b54f33a46809686b60541e
kariumi <kariumi1028a@gmail.com>
2023-04-15 23:26:00 +0900

    as

--- discordbot.py
+++ discordbot.py
@@ -898,7 +898,7 @@ async def on_voice_state_update(member, before, after):
     DBmes = await DBch.fetch_message(1096723359045255238)
     auths = DBmes.content.split("\n")
 
-    await printLog(client, f"memberNAME:{member.name}, beforeVC:{before.channel.name}, afterVC:{after.channel.name}")
+    await printLog(client, f"memberNAME:{member.name},afterVC:{after.channel.name}")
 
     # 作業部屋の通知チャンネル
     ITCguild = client.get_guild(377392053182660609)
